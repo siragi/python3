9d82dbb60e72f9e9285ecd9b6668c207307f703b
simon <simon.giger@tuta.io>
2017-12-03 14:31:48 +0100

    erstellt durch ENEA!

---
+++ Rechner.py
@@ -0,0 +1,5 @@
+ersteZahl = input("Welche Zahl ist die erste Zahl der Rechnung?")
+grundrechenart = input("*, /, + oder -")
+zweiteZahl = input("Welche Zahl ist die zweite Zahl der Rechnung?")
+rechnung = (ersteZahl + grundrechenart + zweiteZahl)
+print (rechnung)
