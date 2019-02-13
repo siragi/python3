c04cec292cf90947b35dc7eae19f1e2feff7514c
Simon Giger <simon.giger@tuta.io>
2019-02-13 22:56:23 +0100

    - input checks to minimize possible damage by eval():   - not isdigit() check (int or float)   - not in "/*-+" - use of exceptions

--- Snippets/input_Rechner.py
+++ Snippets/input_Rechner.py
@@ -1,6 +1,50 @@
 """Zahlen zusammenrechnen."""
-ersteZahl = input("Welche Zahl ist die erste Zahl der Rechnung?")
-grundrechenart = input("*, /, + oder -")
-zweiteZahl = input("Welche Zahl ist die zweite Zahl der Rechnung?")
-rechnung = (ersteZahl + grundrechenart + zweiteZahl)
-print(eval(rechnung))
+# from ast import literal_eval (kann leider nur + und -)
+# Author: Enea Giger
+
+def isdigit(x):
+    try:
+        float(x)
+        return True
+    except ValueError:
+        return False
+
+
+def startRechner():
+    """Starte Rechner."""
+    try:
+        ersteZahl = input(
+            "Welche Zahl ist die erste Zahl der Rechnung? ")
+        while not isdigit(ersteZahl):
+            ersteZahl = input("Eine Zahl bitte ")
+
+        grundrechenart = input(
+            "Bitte Grundrechenart eingeben (* / + - ) ")
+        while grundrechenart not in "*/+-":
+            grundrechenart = input("Wähle * / + - ")
+
+        zweiteZahl = input(
+            "Welche Zahl ist die zweite Zahl der Rechnung? ")
+        while not isdigit(zweiteZahl):
+            zweiteZahl = input("Eine Zahl bitte ")
+
+        try:
+            rechnung = ersteZahl + grundrechenart + zweiteZahl  # Stringconcat!
+            resultat = eval(rechnung)
+        except ZeroDivisionError:
+            zweiteZahl = input("Anderer Divisor bitte, nicht 0 ")
+            while not isdigit(zweiteZahl):
+                zweiteZahl = input("Eine Zahl bitte ")
+            rechnung = ersteZahl + grundrechenart + zweiteZahl  # Strings only!
+            resultat = eval(rechnung)
+        print(rechnung, '=')
+        print("Resultat:", resultat)
+
+        again = input("==================== nochmal? (J/n) ")
+        if again not in ['n', 'N']:
+            startRechner()
+    except (TypeError, ValueError) as e:
+        print(e)
+
+
+startRechner()
